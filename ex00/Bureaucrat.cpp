b042864f42426972f6881fc702288a025f233464
macbook <macbook@macbooks-MacBook-Pro.local>
2025-04-09 02:41:27 +0200

    add: start ex00

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,69 @@
+#include "Bureaucrat.hpp"
+
+// Constructors
+
+Bureaucrat::Bureaucrat() : _name("Bureaucrat"), _grade(150)
+{
+	std::cout << "Default Bureaucrat Constructor Called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : _name(name)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	if (grade > 150)
+		throw GradeTooLowException();
+	this->grade = grade;
+	std::cout << "Bureaucrat " << this->name << " constructed with grade " << grade << "." << std::endl;
+}
+
+// Bureaucrat::~Bureaucrat()
+// {
+// 	std::cout << "Bureaucrat with name of " << name << " is destroyed." << std::endl;
+// }
+
+// Animal &Animal::operator=(const Animal &copy)
+// {
+// 	if (this != &copy)
+// 	{
+// 		type = copy.type;
+// 	}
+// 	std::cout << "Assignment was made to Animal with type of " << type << "." << std::endl;
+// 	return (*this);
+// }
+
+// Animal::Animal(const Animal &copy) : type(copy.type)
+// {
+// 	*this = copy;
+// 	std::cout << "Animal with type of " << type << " was copied." << std::endl;
+// };
+// // Member Functions
+// void Animal::makeSound() const
+// {
+// 	std::cout << "***MAKING ANIMAL SOUNDS***" << std::endl;
+// }
+
+// std::string Animal::getType(void) const
+// {
+// 	return (type);
+// }
+
+void	Bureaucrat::setGrade(int grade)
+{
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	else if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else
+		this->_grade = grade;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade is too high!");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade is too low!");
+}
---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <cctype>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <string>
+
+class Bureaucrat
+{
+  private:
+	const std::string _name;
+	int _grade;
+    void setGrade(int grade);
+
+  public:
+	// Constructors
+	Bureaucrat();
+	Bureaucrat(const std::string &name, int grade);
+
+	// Exception classes
+	class GradeTooHighException : public std::exception
+	{
+		public:
+		const char *what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+		public:
+		const char *what() const throw();
+	};
+};
